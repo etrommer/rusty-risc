74b4de8dd3bf60a0b98e38472c13687e4f25fb0a
Jadeaffenjaeger <jadeaffenjaeger@gmx.net>
2025-08-02 09:50:31 +0200

    Add trap definitions

---
+++ src/trap.rs
@@ -0,0 +1,30 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum RVException {
+    InstructionAddressMisaligned(usize),
+    InstructionAccessFault(usize),
+    IllegalInstruction(u32),
+    BreakPoint,
+    LoadAddressMisaligned(usize),
+    LoadAccessFault(usize),
+    StoreAddressMisaligned(usize),
+    StoreAccessFault(usize),
+    EnvironmentCall,
+    TimerInterrupt,
+}
+
+impl RVException {
+    pub fn to_ecode(self) -> u32 {
+        match self {
+            Self::InstructionAddressMisaligned(_) => 0,
+            Self::InstructionAccessFault(_) => 1,
+            Self::IllegalInstruction(_) => 2,
+            Self::BreakPoint => 3,
+            Self::LoadAddressMisaligned(_) => 4,
+            Self::LoadAccessFault(_) => 5,
+            Self::StoreAddressMisaligned(_) => 6,
+            Self::StoreAccessFault(_) => 7,
+            Self::EnvironmentCall => 11,
+            Self::TimerInterrupt => 0x8000_0007,
+        }
+    }
+}
